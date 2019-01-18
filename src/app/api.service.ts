0134a3a684530e6319ebced35b09a66ac3366fe0
Ruben <rubendiazee@gmail.com>
2019-01-18 13:04:21 +0100

    login logic done. remember post /users/login needs to return always json

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,17 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { promise } from 'protractor';
+import { resolve, reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
+jwt: string;
 
 
   constructor(private http:HttpClient) { }
 
   register(username,password){
-    return this.http.post('https://apitrello.herokuapp.com/users',{username, password}).toPromise()
-   
+    return this.http.post('https://apitrello.herokuapp.com/users',{username, password}).toPromise();  
+  }
+  login(username,password){
+    return new Promise((resolve, reject)=>{
+    this.http.post('https://apitrello.herokuapp.com/users/login',{username, password})
+    .toPromise()
+    .then( result =>{
+      console.log('then', result);
+      reject(204);
+    }).catch(maybeNotAndError =>{
+      console.log('catch', maybeNotAndError);
+      if(maybeNotAndError.status === 200){
+        const jwt=maybeNotAndError.error.text;
+        this.jwt = jwt;
+        resolve('user not found');
+      } else if(maybeNotAndError.status === 401){
+        reject('wrong pass');
+      } 
+      else {
+        reject('try again');
+      }
+    });
+  }); 
   }
 }
--- src/app/login-view/login-view.component.ts
+++ src/app/login-view/login-view.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-view',
   templateUrl: './login-view.component.html',
   styleUrls: ['./login-view.component.scss']
 })
-export class LoginVIewComponent implements OnInit {
+export class LoginVIewComponent  {
 
-  constructor() { }
+  username: string;
+  password: string;
+  error: any;
+  valid: any;
 
-  ngOnInit() {
+  constructor(private api: ApiService, private router: Router) { }
+
+  login(){
+    const {username, password} = this;
+    if(this.username.trim() !== '' && password.trim() !== ''){
+      this.api.
+      login(username.trim(), password.trim())
+      .then(() =>{
+        // console.log('then',res);
+        this.error = undefined;
+        this.router.navigate(['/board']);
+       // this.valid = res;
+      })
+      .catch(error =>{
+        console.log('catch',error);
+
+        this.error=error;
+      });
+    }
   }
+ 
 
 }
